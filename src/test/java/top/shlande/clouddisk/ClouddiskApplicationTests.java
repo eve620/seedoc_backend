840766b09dbfdc2e6098fde4f2ab9cc67e8b579e
shlande <shlandeme@gmail.com>
2023-03-30 17:37:01 +0800

    init

---
+++ src/test/java/top/shlande/clouddisk/ClouddiskApplicationTests.java
@@ -0,0 +1,13 @@
+package top.shlande.clouddisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouddiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
