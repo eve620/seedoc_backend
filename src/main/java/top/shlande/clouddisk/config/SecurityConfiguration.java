dba428474bc5a5741187ef6fe6fee3eab2c7bd44
shlande <shlandeme@gmail.com>
2023-04-10 00:15:21 +0800

    使用shiro重写权限

--- src/main/java/top/shlande/clouddisk/config/SecurityConfiguration.java
+++ src/main/java/top/shlande/clouddisk/config/SecurityConfiguration.java
@@ -6,12 +6,16 @@ import org.apache.shiro.realm.Realm;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.util.JdbcUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import top.shlande.clouddisk.user.jdbc.JdbcUserRepository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -24,12 +28,17 @@ public class SecurityConfiguration {
     // Realm is a bridge for shiro to access security data
     // https://www.infoq.com/articles/apache-shiro/
     @Bean
-    public Realm realm(@Autowired DataSource dataSource) {
+    public Realm realm(@Autowired DataSource dataSource, JdbcUserRepository repository) {
         var realm = new JdbcRealm() {
             @Override
             protected Set<String> getPermissions(Connection conn, String username, Collection<String> roleNames) throws SQLException {
-                // fix permission
-                return new HashSet<>();
+                // 这里 roleNames 不会被用啦控制权限，只会参考 permission 字段
+                var userOptional = repository.findById(username);
+                if (userOptional.isEmpty()) {
+                    return null;
+                }
+                var user = userOptional.get();
+                return new LinkedHashSet<>(List.of(user.permission));
             }
         };
         realm.setUserRolesQuery(userRoleQuery);
