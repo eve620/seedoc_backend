f88b0dd1a08bbe9d3c5c09e1dbddcd6bd607f66e
shlande <shlandeme@gmail.com>
2023-04-15 16:03:14 +0800

    使用和springboot集成的cas

--- src/main/java/top/shlande/clouddisk/CloudDiskApplication.java
+++ src/main/java/top/shlande/clouddisk/CloudDiskApplication.java
@@ -1,10 +1,12 @@
 package top.shlande.clouddisk;
 
+import org.apereo.cas.client.boot.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableCasClient
 public class CloudDiskApplication {
 
     public static void main(String[] args) {
